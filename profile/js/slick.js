f50eec72b2569c49658438404bd115ad51d3a688
hyukmoonp <hyukmoonp@gmail.com>
2022-06-30 10:59:57 +0900

    slick_function added

---
+++ profile/js/slick.js
@@ -0,0 +1,11 @@
+$(function () {
+  $(".slider-wrap").slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+  });
+});
